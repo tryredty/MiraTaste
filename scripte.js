b1352c62965198925a0a7df1cf6d97c033c52e2f
tryredty <aouissifousseni@gmail.com>
2025-04-22 21:38:03 +0200

    Update scripte.js

--- scripte.js
+++ scripte.js
@@ -1,6 +1,6 @@
 // script.js
 function contactWhatsApp(message) {
-  const phone = '22892512219'; // Remplace par le numéro WhatsApp de la pâtissière
+  const phone = '22892944771'; // Remplace par le numéro WhatsApp de la pâtissière
   const encodedMessage = encodeURIComponent(message);
   const url = `https://wa.me/${phone}?text=${encodedMessage}`;
   window.open(url, '_blank');
